508a8bfd626f274d827825a2e2696df4ec29b094
12345678 <pragati241299@gmail.com>
2020-08-12 10:48:56 +0530

    changes'

--- src/app/shared/faq/faq.component.ts
+++ src/app/shared/faq/faq.component.ts
@@ -6,7 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./faq.component.scss']
 })
 export class FaqComponent implements OnInit {
- 
+  faqlist=[
+    {
+      title:'What is TheSteelStore?',
+      description: 'What do you sell?',
+      descriptiondetail:'We sell prime, excess prime, and secondary industrial materials. Our prime material comes from the original manufacturers. We buy direct from vetted and approved global manufacturers. Our excess prime is also prime material and it comes from our large “circulating” supplier base. This is material for which its current owner doesn'
+    },
+    {
+      title:'What is TheSteelStore?',
+      description: 'What do you sell?',
+      descriptiondetail:'We sell prime, excess prime, and secondary industrial materials. Our prime material comes from the original manufacturers. We buy direct from vetted and approved global manufacturers. Our excess prime is also prime material and it comes from our large “circulating” supplier base. This is material for which its current owner doesn'
+    },
+   
+    ]
+
 
   constructor( ) { 
     
