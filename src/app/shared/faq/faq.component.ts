e3d7cc88dc389e7eacb8dea7bab0966e6b1d6e4e
12345678 <pragati241299@gmail.com>
2020-08-12 16:02:53 +0530

    update page

--- src/app/shared/faq/faq.component.ts
+++ src/app/shared/faq/faq.component.ts
@@ -17,6 +17,26 @@ export class FaqComponent implements OnInit {
       description: 'What do you sell?',
       descriptiondetail:'We sell prime, excess prime, and secondary industrial materials. Our prime material comes from the original manufacturers. We buy direct from vetted and approved global manufacturers. Our excess prime is also prime material and it comes from our large “circulating” supplier base. This is material for which its current owner doesn'
     },
+    {
+      title:'What is TheSteelStore?',
+      description: 'What do you sell?',
+      descriptiondetail:'We sell prime, excess prime, and secondary industrial materials. Our prime material comes from the original manufacturers. We buy direct from vetted and approved global manufacturers. Our excess prime is also prime material and it comes from our large “circulating” supplier base. This is material for which its current owner doesn'
+    },
+    {
+      title:'What is TheSteelStore?',
+      description: 'What do you sell?',
+      descriptiondetail:'We sell prime, excess prime, and secondary industrial materials. Our prime material comes from the original manufacturers. We buy direct from vetted and approved global manufacturers. Our excess prime is also prime material and it comes from our large “circulating” supplier base. This is material for which its current owner doesn'
+    },
+    {
+      title:'What is TheSteelStore?',
+      description: 'What do you sell?',
+      descriptiondetail:'We sell prime, excess prime, and secondary industrial materials. Our prime material comes from the original manufacturers. We buy direct from vetted and approved global manufacturers. Our excess prime is also prime material and it comes from our large “circulating” supplier base. This is material for which its current owner doesn'
+    },
+    {
+      title:'What is TheSteelStore?',
+      description: 'What do you sell?',
+      descriptiondetail:'We sell prime, excess prime, and secondary industrial materials. Our prime material comes from the original manufacturers. We buy direct from vetted and approved global manufacturers. Our excess prime is also prime material and it comes from our large “circulating” supplier base. This is material for which its current owner doesn'
+    },
    
     ]
 
